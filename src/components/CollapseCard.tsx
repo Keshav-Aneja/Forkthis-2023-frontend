8068d8730b9ecbe9a92470bf6de35ad1d083d6ff
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2023-08-22 22:56:39 +0530

    update:used table instead of flexbox

--- src/components/CollapseCard.tsx
+++ src/components/CollapseCard.tsx
@@ -4,10 +4,36 @@ import React, { useState } from 'react';
 import { Collapse } from 'react-collapse';
 
 export default function CollapseCard() {
+  const arrRepo=[
+  {
+    srNo:1,
+    desctiption:"Issue Desciption",
+    difficulty:"Easy",
+    points:5
+  },
+  {
+    srNo:1,
+    desctiption:"Issue Desciption",
+    difficulty:"Mid",
+    points:5
+  },
+  {
+    srNo:1,
+    desctiption:"Issue Desciption",
+    difficulty:"Hard",
+    points:5
+  },
+  {
+    srNo:1,
+    desctiption:"Issue Desciption",
+    difficulty:"Hard",
+    points:5
+  }]
+
   const [isCollapsed, setIsCollapsed] = useState(true);
   const theme= {
     collapse: 'ReactCollapse--collapse  w-full transition-all duration-500',
-     content: 'ReactCollapse--content w-full'
+     content: 'ReactCollapse--content w-full flex justify-around items-center'
   }
   const [rotationAngle, setRotationAngle] = useState(0);
 
@@ -17,9 +43,10 @@ export default function CollapseCard() {
 
 
   return (
-    <section className='bg-[#0A070E] flex justify-around items-center py-[5px] flex-col '> 
-    <section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
-    <p>Sr.No</p>
+    <section className='bg-[#0A070E] flex justify-around items-center py-[5px] flex-col '>
+      <section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
+      <section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
+      <p>Sr.No</p>
       <h1>Repo Name</h1>
       <h3>Tech Stack</h3>
       <p>Issues</p>
@@ -28,59 +55,31 @@ export default function CollapseCard() {
         <Image width={16} height={16} alt="arrowButton" src="/dashboard/arrow.svg" onClick={rotateImage}  style={{ transform: `rotate(${rotationAngle}deg)` }} className="duration-300 transition-all"/>
       </div>
     </section>
-     <Collapse isOpened={!isCollapsed} theme={
-      theme
-     }>
 
-     <section className='flex flex-row justify-around'>
-      
-     <section className='flex flex-col w-[75%] bg-gray-500 rounded-[14px]' id='issuesContainer'>
-{/*repo section header */}
-
-<section className=' flex justify-between items-center py-[5px] px-8 w-full'>
-<p>Sr.No h</p>
-<h1>Repo Name h</h1>
-<h3>Tech Stack h</h3>
-<p>Issues h</p>
-</section>
-
-{/*repo section */}
-
-<section className='flex flex-col w-full'>
-<section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
-<p>Sr.No</p>
-<h1>Repo Name</h1>
-<h3>Tech Stack</h3>
-<p>Issues</p>
-</section>
-<section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
-<p>Sr.No</p>
-<h1>Repo Name</h1>
-<h3>Tech Stack</h3>
-<p>Issues</p>
-</section>
-<section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
-<p>Sr.No</p>
-<h1>Repo Name</h1>
-<h3>Tech Stack</h3>
-<p>Issues</p>
-</section>
-<section className='bg-[#0A070E] flex justify-between items-center py-[5px] px-8 w-full'>
-<p>Sr.No</p>
-<h1>Repo Name</h1>
-<h3>Tech Stack</h3>
-<p>Issues</p>
-</section>
-</section>
-
-</section>
-
-<Image width={100} height={100} alt="a" src="/dashboard/arrow.svg"/>
-
-
-     </section>
-     </Collapse>
-
+      </section>
+      <Collapse isOpened={!isCollapsed} theme={theme}>
+        <table className=' w-[75%] bg-gray-500 rounded-[14px]' id='issuesContainer'>
+          <thead>
+            <tr >
+              <th>Sr.No</th>
+              <th>Description</th>
+              <th>Difficulty</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody >
+            {arrRepo.map((repo) => (
+              <tr key={repo.srNo}>
+                <td className='text-center'>{repo.srNo}</td>
+                <td className='text-center'>{repo.desctiption}</td>
+                <td className='text-center'>{repo.difficulty}</td>
+                <td className='text-center'>{repo.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Image width={100} height={100} alt="a" src="/dashboard/arrow.svg" />
+      </Collapse>
     </section>
   );
 }
