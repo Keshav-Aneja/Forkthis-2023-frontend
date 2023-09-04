f97ae2757352f5e3ba603d7f7925bc1d6500f7cc
Keshav-Aneja <keshav.aneja09@gmail.com>
2023-09-04 18:19:57 +0530

    Fix: Sidebar

--- src/components/Sidebarrr.tsx
+++ src/components/Sidebarrr.tsx
@@ -5,10 +5,8 @@ import { useMediaQuery } from "@mui/material";
 import Link from "next/link";
 import Resources from "../app/resources/page";
 import Image from "next/image";
-
 export default function Sidebarrr() {
-  const isDesktop = useMediaQuery("(max-width: 500px)");
-  const [menuOpened, setMenuOpened] = useState(!isDesktop);
+  const [menuOpened, setMenuOpened] = useState(true);
 
   const toggleMenu = () => {
     setMenuOpened((prevMenuOpened) => !prevMenuOpened);
@@ -16,7 +14,9 @@ export default function Sidebarrr() {
 
   return (
     <div
-      className={`mainNav bg-[#212426] fixed w-[100%] rounded-br-2xl rounded-bl-2xl h-12 p-2 ${
+      className={`mainNav ${
+        menuOpened ? "bg-[#212426] duration-0" : "bg-[rgba(0,0,0,0)"
+      } md:bg-[#212426] fixed w-[100%] rounded-br-2xl rounded-bl-2xl h-12 p-2 ${
         menuOpened ? "h-fit pb-5 p-2" : ""
       } md:h-16 md:flex md:items-center md:justify-center md:text-lg lg:rounded-br-2xl lg:h-screen lg:w-[7%]
        lg:rounded-tr-2xl lg:rounded-bl-none lg:p-0 lg:hover:w-[8%] duration-300 ease-linear z-50 lg:fixed`}
@@ -29,21 +29,26 @@ export default function Sidebarrr() {
         </svg>
       </div>
       <div
-        className={`nav flex justify-center items-center flex-col gap-5 md:visible lg:visible md:flex-row md:justify-around md:w-[100%] md:items-baseline lg:flex-col lg:text-xs lg:h-[100%] lg:gap-0 ${
-          menuOpened ? "block" : "hidden"
-        }`}
+        className={`nav justify-center items-center ${
+          menuOpened ? "flex" : "hidden"
+        } md:flex flex-col gap-5 lg:flex md:flex-row md:justify-around md:w-[100%] md:items-baseline lg:flex-col lg:text-xs lg:h-[100%] lg:gap-0`}
       >
-        <div className="home w-[60%] flex justify-between gap-5 items-center md:justify-center md:w-max md:gap-2 lg:flex-col lg:py-12 lg:gap-3 lg:w-[100%] lg:h-[30%] lg:hover:bg-black lg:px-5  lg:hover:text-sm duration-300">
-          <Image
-            className="md:w-[1.5rem] lg:w-[2.2rem]"
-            src="/dashboard/userIcon.png"
-            width={50}
-            height={50}
-            alt="Picture of the author"
-          />
-          <span className="font-gilroyRegular font-bold">Home</span>
-        </div>
-
+        {" "}
+        <Link
+          href="/dashboard"
+          className="w-full md:w-[30%] lg:w-full flex justify-center gap-5 items-center md:justify-center md:gap-2 lg:flex-col lg:h-[33.3%]  lg:hover:bg-black  lg:hover:text-sm duration-300"
+        >
+          <div className="home w-[60%] flex justify-between gap-5 items-center md:justify-center md:w-max md:gap-2 lg:flex-col lg:py-12 lg:gap-3 lg:w-[100%] lg:h-[30%] lg:hover:bg-black lg:px-5  lg:hover:text-sm duration-300">
+            <Image
+              className="md:w-[1.5rem] lg:w-[2.2rem]"
+              src="/dashboard/userIcon.png"
+              width={50}
+              height={50}
+              alt="Picture of the author"
+            />
+            <span className="font-gilroyRegular font-bold">Home</span>{" "}
+          </div>{" "}
+        </Link>
         <div
           className="w-full h-1 sm:hidden lg:block p-0 m-0"
           style={{
@@ -79,7 +84,7 @@ export default function Sidebarrr() {
           </div>
 
           <Link
-            href="/Resources"
+            href="/resources"
             className="w-full flex justify-between gap-5 items-center md:justify-center md:gap-2 lg:flex-col lg:h-[33.3%]  lg:hover:bg-black  lg:hover:text-sm duration-300"
           >
             <Image
