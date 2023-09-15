662ce57726684232237fb556c52af05cc6e8a128
Keshav-Aneja <keshav.aneja09@gmail.com>
2023-09-15 21:04:09 +0530

    feat. Landing Page Desktop Version Complete

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,9 @@
 import FAQ from "../../sections/FAQ";
+import Landing from "../../sections/Landing";
 export default function page() {
   return (
     <div>
+      <Landing />
       <FAQ></FAQ>
     </div>
   );
---
+++ src/sections/Landing.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Landing() {
+  return (
+    <div className=" w-full overflow-x-hidden">
+      <div className="circle-bg absolute overflow-x-hidden w-full -z-10">
+        <div className="circle w-[150vw] h-[150vw] border-[80px] border-[#4a18bd] rounded-full relative top-[-105vw] left-[-25vw] blur-xl"></div>
+        <div className="circle w-[150vw] h-[150vw] border-[40px] border-[#8A61FF] rounded-full absolute top-[-103vw] left-[-25vw] blur-xl"></div>
+      </div>
+      <Nav></Nav>
+      <div className="w-full h-[90vh] overflow-x-hidden flex flex-col justify-center items-center gap-10 font-gilroyBlack">
+        <div className="heading text-center text-7xl font-extrabold">
+          Welcome To ForkThis
+        </div>
+        <div className="sub-heading text-center italic font-gilroyRegular text-xl font-bold">
+          Git.Set.Fork!
+        </div>
+        <div className="reg-button py-2 px-12 text-black bg-[#8A61FF] text-xl  rounded-xl font-bolder hover:text-[#af99ed] cursor-pointer hover:bg-[#4a18bd] transition-all duration-300">
+          Register
+        </div>
+      </div>
+    </div>
+  );
+}
+function Nav() {
+  return (
+    <div className="w-full h-20 flex justify-center sticky top-0 left-0">
+      <div className="nav-container w-[80%] flex justify-between ">
+        <div className="left flex gap-8 items-center font-gilroyBlack font-bold text-xl">
+          <Image
+            width={50}
+            height={50}
+            alt="arrowButton"
+            src="/images/logo.png"
+            style={{
+              width: "60px",
+              height: "60px",
+            }}
+            className="duration-300 transition-all"
+          />
+          <Link href="">About</Link>
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/Resources">Resources</Link>
+        </div>
+        <div className="right flex gap-8 items-center font-gilroyBlack text-xl">
+          <div className="login-btn underline-offset-2 underline cursor-pointer">
+            Login
+          </div>
+          <div className="register-btn py-2 px-6 bg-[#8A61FF] rounded-full text-black cursor-pointer hover:text-[#af99ed] hover:bg-[#4a18bd] transition-all duration-300">
+            Register
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
