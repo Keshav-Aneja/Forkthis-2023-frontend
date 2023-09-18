f2cf75b2380013c7c439ef263ee04d6ba98955cc
Keshav-Aneja <keshav.aneja09@gmail.com>
2023-09-18 22:38:24 +0530

    UI changes

--- src/sections/FAQ.tsx
+++ src/sections/FAQ.tsx
@@ -36,7 +36,7 @@ const faqData = [
 import FAQList from "../components/FAQList";
 export default function FAQ() {
   return (
-    <div className="faq-container z-10 w-full min-h-[100vh] h-full flex justify-center items-center">
+    <div className="faq-container z-10 w-full min-h-[100vh] h-full flex justify-center items-center lg:mt-24">
       <div className="w-[100%] h-fit p-4 md:p-24 pt-10 md:pt-20 bg-[#8A61FF33] flex flex-col">
         <div className="heading text-white text-4xl font-gilroyBlack font-bolder mb-4 text-center md:text-left">
           FAQs
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,34 +3,28 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-between items-center flex-col w-full  gap-[1.5rem] text-gray-300 bg-[#8A61FF33] px-[1rem] py-[2rem]">
-        {/* add  logo here */}
-        <div className="flex justify-between items-center w-[12%] ">
-            <Image width={20} height={20} src="/sampleLogo.svg" alt="Logo"/>
-            <h1 className="font-gilroyBlack text-4xl">ForkThis</h1>
-        </div>
+    <footer className="flex justify-between items-center flex-col w-full  gap-[1.5rem] text-gray-300 bg-[#8A61FF33] px-[1rem] py-[2rem] mt-16">
+      {/* add  logo here */}
+      <div className="flex justify-center items-center w-[12%] ">
+        <Image width={20} height={20} src="/sampleLogo.svg" alt="Logo" />
+        <h1 className="font-gilroyBlack text-4xl text-center">ForkThis</h1>
+      </div>
 
-        <div className="flex  w-[30%] text-xl justify-between items-center font-gilroyRegular">
-<p>First Link</p>
-<p>First Link</p>
-<p>First Link</p>
-<p>First Link</p>
-        </div>
-        <div className="flex  w-[30%] text-xl justify-between items-center font-gilroyRegular">
-            
-        </div>
+      <div className="flex  w-[30%] text-xl justify-center items-center font-gilroyRegular">
+        <p>First Link</p>
+        <p>First Link</p>
+        <p>First Link</p>
+        <p>First Link</p>
+      </div>
+      <div className="flex  w-[30%] text-xl justify-between items-center font-gilroyRegular"></div>
 
-        <div className="flex justify-between items-center">
-        <Image width={32} height={32} src="/images/1.svg" alt="Logo"/>
-        <Image width={32} height={32} src="/images/2.svg" alt="Logo"/>
-        <Image width={32} height={32} src="/images/3.svg" alt="Logo"/>
-        <Image width={32} height={32} src="/images/4.svg" alt="Logo"/>
-        </div>
-        <p>
-            6423 All rights Reserved.
-        </p>
-
-
+      <div className="flex justify-between items-center">
+        <Image width={32} height={32} src="/images/1.svg" alt="Logo" />
+        <Image width={32} height={32} src="/images/2.svg" alt="Logo" />
+        <Image width={32} height={32} src="/images/3.svg" alt="Logo" />
+        <Image width={32} height={32} src="/images/4.svg" alt="Logo" />
+      </div>
+      <p>6423 All rights Reserved.</p>
     </footer>
   );
 }
