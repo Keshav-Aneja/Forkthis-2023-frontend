3b8f53e877080bc8ef80bef284d89090d653fb34
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2023-09-21 16:09:38 +0530

    updated

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -4,9 +4,10 @@ import Cardbox from "@/components/Cardbox";
 
 export default function Resources() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden h-screen relative">
       <Sidebarrr />
       {/*  */}
+      <h1 className="absolute top-[50%] left-[50%] translate-x-[-40%] font-gilroyBlack text-5xl">Coming Soon</h1>
     </div>
   );
 }
