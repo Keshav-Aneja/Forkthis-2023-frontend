750de14671d91bafe2e9f298685ca2f38323a4d6
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-02-06 20:45:22 +0530

    fix: access for other pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,13 +9,13 @@ import { toast } from "react-toastify";
 
 export default function Page() {
   const router = useRouter();
-  useEffect(() => {
-    const token = Cookie.get("token");
-    if (!token) {
-      router.push("/");
-      toast.error("Kindly sign in with Github", { theme: "dark" });
-    }
-  }, []);
+  // useEffect(() => {
+  //   const token = Cookie.get("token");
+  //   if (!token) {
+  //     router.push("/");
+  //     toast.error("Kindly sign in with Github", { theme: "dark" });
+  //   }
+  // }, []);
 
   return (
     <div className="flex w-[100%] z-0">
